a153f751c886c778de9f087613d4a8f2439c7b33
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-04-06 14:36:30 +0700

    Install react-redux and redux

---
+++ src/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+} from 'react-native'
+
+class Main extends Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    return (
+      <View>
+            
+      </View>
+    )
+  }
+}
+
+export default Main
